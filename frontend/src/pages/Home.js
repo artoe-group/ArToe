57b9b63bad39e855408a3763b81d683d545bdd18
ArToe Group <magezashalom0@gmail.com>
2024-11-22 05:43:35 +0000

    CHNGS

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import products from '../data/products.json';
-import ProductCard from '../components/ProductCard';
+import { Link } from 'react-router-dom';
+import './Home.css'; 
 
 const Home = () => {
-  return (
-    <div style={styles.container}>
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
-  );
-};
-
-const styles = {
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    gap: '20px',
-    justifyContent: 'center',
-    marginTop: '20px',
-  },
+    return (
+        <div className="home">
+            <header className="header-container">
+                <div className="header-info">
+                    <h1>ArToe</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+                    <Link to="/sneakers" className="store-button">Cop One</Link>
+                </div>
+                <div className="header-image">
+                    <video src="/images/Comercial.mp4" autoPlay loop muted className="header-video" />
+                </div>
+            </header>
+        </div>
+    );
 };
 
 export default Home;
+
+
